1b6f8d18e304d4d0650e2d1f8ce24d1c67a7ae91
Maksym Makarov <Maksym_Makarov@epam.com>
2019-05-29 11:30:47 +0200

    implement select, refresh, remove methods

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,6 @@
-window.onload = window => {
+window.onload = () => {
+
+  // initialization
   ws.setOptions({
     form: document.querySelector("form"),
     table: document.querySelector("table")
@@ -6,11 +8,26 @@ window.onload = window => {
 
   ws.refresh();
 
+  // add event linstener to submit button
   document.querySelector("#submit").addEventListener("click", e => {
     e.preventDefault();
-
     ws.save();
-    ws.clearFields();
+    ws.clearForm();
+    ws.refresh();
+  });
+
+  // add event linstener to table row
+  document.querySelector("table").addEventListener("dblclick", ws.select);
+
+  // add event linstener to delete button
+  document.querySelector("#deleteBtn").addEventListener("click", e => {
+    ws.remove("tr.row-select");
+    ws.refresh();
+  });
+
+  // add event linstener to clear button
+  document.querySelector("#clearBtn").addEventListener("click", e => {
+    ws.remove("tr");
     ws.refresh();
   });
 };
--- app/js/ws-module.js
+++ app/js/ws-module.js
@@ -1,4 +1,8 @@
 const ws = (function() {
+  const _options = {};
+  const _tableHeaderNodeElement = document.querySelector("thead");
+  const _tableBodyNodeElement = document.querySelector("tbody");
+
   const _defaults = {
     form: null,
     table: null,
@@ -7,44 +11,105 @@ const ws = (function() {
     seelctedRowClass: "row-select"
   };
 
-  const _options = {};
+  const _api = {
+    //   exeption QUOTA_EXCEEDED_ERR
+    get(key) {
+      try {
+        const data = localStorage.getItem(key);
+        return JSON.parse(data);
+      } catch (ex) {
+        console.error(ex);
+      }
+    },
+    set(key, value) {
+      try {
+        localStorage.setItem(key, JSON.stringify(value));
+      } catch (ex) {
+        console.error(ex);
+      }
+    },
+    remove(key) {
+      try {
+        localStorage.removeItem(key);
+      } catch (ex) {
+        console.error(ex);
+      }
+    }
+  };
 
-  return {
+  function _isValidInputValue(val) {
+    if (!val.trim().length) {
+      console.error("Save data was failed. Please, fill in ALL form fields!");
+      return false;
+    }
+    return true;
+  }
+
+  function _getFormDataByFields(fieldsList) {
+    let data = {};
+    for (let fieldId of fieldsList) {
+      let val = _options.form.querySelector(`#${fieldId}`).value;
+      if (!_isValidInputValue(val)) return;
+      data[fieldId] = val;
+    }
+    return data;
+  }
+
+  function _renderTable(key, data) {
+    const rowNodeElement = document.createElement("tr");
+    rowNodeElement.dataset.key = key;
+
+    for (let item in data) {
+      const cell = document.createElement("td");
+      cell.innerText = data[item];
+      rowNodeElement.appendChild(cell);
+    }
+    _tableBodyNodeElement.appendChild(rowNodeElement);
+  }
+
+  return Object.freeze({
     setOptions(obj) {
       Object.assign(_options, _defaults, obj);
     },
 
     save() {
-      let data = {},
-        key;
-
-      for (let fieldId of _options.fieldsId) {
-        let val = _options.form.querySelector(`#${fieldId}`).value;
-        if (!val.trim().length) {
-          console.error("Save data was failed. Please, fill in ALL form fields!");
-          return;
-        }
-        data[fieldId] = val;
-      }
-
-      try {
-        key = _options.keyPref + Date.now();
-        localStorage.setItem(key, JSON.stringify(data));
-      } catch (ex) {
-        console.log(ex);
-      }
+      const data = _getFormDataByFields(_options.fieldsId);
+      let key = _options.keyPref + Date.now();
+      _api.set(key, data);
     },
 
-    clearFields() {
+    clearForm() {
       _options.form.reset();
     },
 
     refresh() {
-      let tableHeaderNodeElement = _options.table.querySelector("thead");
-
-      tableHeaderNodeElement.innerHTML = _options.fieldsId
+      _tableBodyNodeElement.innerHTML = null;
+      _tableHeaderNodeElement.innerHTML = _options.fieldsId
         .map(fieldId => `<th>${fieldId}</th>`)
         .join("");
+
+      for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if (!key.includes(_options.keyPref)) continue;
+
+        const person = _api.get(key);
+        _renderTable(key, person);
+      }
+    },
+
+    select(e) {
+      if (e.target.tagName !== "TD") return;
+      e.target.parentNode.classList.toggle(_options.seelctedRowClass);
+    },
+
+    remove(selector) {
+      const selectedRows = _tableBodyNodeElement.querySelectorAll(
+        `${selector}`
+      );
+      for (let row of selectedRows) {
+        const key = row.dataset.key;
+        _api.remove(key);
+      }
     }
-  };
+  });
 })();
